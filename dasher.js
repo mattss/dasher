1e06fbbc23d934f9aad8e5b3a298d91b07d05ed3
Matthew Sital-Singh <mattss@netsight.co.uk>
2017-11-23 15:05:02 +0000

    Add light control

--- dasher.js
+++ dasher.js
@@ -1,9 +1,16 @@
 const DashButton = require('dash-button');
+var request = require('request');
 
 const DASH_BUTTON_MAC_ADDRESS = '88:71:e5:e0:a9:10';
 
 let button = new DashButton(DASH_BUTTON_MAC_ADDRESS);
 
 let subscription = button.addListener(async () => {
-    console.log('ELLO');
+    console.log('buuttooon');
+    request.post(
+        'http://rey.local:8123/api/services/light/turn_off',
+        { json: {
+            entity_id: "light.porch"
+        }}
+    );
 });
